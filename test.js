062c6a681ea3749788b618daf37ed817e4182a64
ImadMebrouk <mebrouk.imad@gmail.com>
2019-01-04 12:27:35 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,38 @@
+
+var MongoClient = require("mongodb").MongoClient;
+var assert = require('assert');
+
+ let uri = 'mongodb://Admin:root95@ds137634.mlab.com:37634/nba';
+
+// Database Name
+const dbName = 'nba';
+
+// Create a new MongoClient
+const client = new MongoClient(uri);
+
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('Actions');
+  // Find some documents
+
+var myrequest = {"TeamName" : "New York Knicks"}
+
+    collection.find(myrequest).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+}
+
+// Use connect method to connect to the Server
+  client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  findDocuments(db, function() {
+    client.close();
+  });
+});
