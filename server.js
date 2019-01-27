ea610dca32062ee4ccacbb4b20cf1d44802b29de
ossama95 <ossama.ougzin@gmail.com>
2019-01-27 14:32:22 +0100

    Add files via upload
    Final

--- server.js
+++ server.js
@@ -149,6 +149,167 @@ db.collection('Actions').mapReduce(
 });
 
 
+app.get("/JoueurSaison", (req, res) => {
+
+	if(req.query.PlayerName) 
+ 	{
+		db.collection('Actions').aggregate([   
+      { $match : { 
+           "PlayerName":req.query.PlayerName
+       }},
+       {
+        $group : {
+           _id : "$PlayerName", 
+           Games: { $sum: 1 },
+           Pts:{$sum:"$Points"},
+           TotalRebounds:{ $sum: "$TotalRebounds" },
+           TotalAssists:{ $sum: "$Assists" },
+           TotalSteals:{ $sum: "$Steals" },
+           TotalBlockedShots:{ $sum: "$BlockedShots" },
+        }
+    },
+     { "$addFields": {
+         Pts:{$sum:"$Points"},
+
+        "Pts Per Game": { "$divide": ["$Pts", "$Games"] },
+        "Rebounds Per Game": { "$divide": ["$TotalRebounds", "$Games"] },
+        "Assists Per Game": { "$divide": ["$TotalAssists", "$Games"] },
+        "Steals Per Game": { "$divide": ["$TotalSteals", "$Games"] },
+        "BlockedShots Per Game": { "$divide": ["$TotalBlockedShots", "$Games"] },
+
+    }}
+    
+ ]).toArray((err, result) => {
+		if (err) return console.log(err)
+		result.SearchedPlayer = req.query.PlayerName;
+		res.render('JoueurSaison.ejs', {Player: result})
+		console.log(result);
+		});
+ 	}
+
+
+ 	else
+ 			{
+    res.render('JoueurSaison.ejs', {Players: [], Player: []})
+
+
+            }
+});
+
+app.get("/EquipeSaison", (req, res) => {
+
+    if(req.query.TeamName)
+        {
+            db.collection('Actions').mapReduce(
+    function () {
+               k = {GameId:this.TeamName};
+       v = {
+           Points:this.Points,
+           OffensiveRebounds:this.OffensiveRebounds,
+           DefensiveRebounds:this.DefensiveRebounds,
+           TotalRebounds:this.TotalRebounds,
+           BlockedShots:this.BlockedShots,
+           ThreePointMade:this.ThreePointMade,
+           };
+       emit(k,v);
+    },
+    function (key, values) {
+    reducedVal = { Points: 0, 
+                   Actions: 0,
+                   OffensiveRebounds:0,
+                   DefensiveRebounds:0,
+                   TotalRebounds:0,
+                   BlockedShots:0,
+                   ThreePointMade:0 };
+    for (var idx = 0; idx < values.length; idx++) {
+     reducedVal.Points += values[idx].Points;
+     reducedVal.OffensiveRebounds += values[idx].OffensiveRebounds;
+     reducedVal.DefensiveRebounds += values[idx].DefensiveRebounds;
+     reducedVal.TotalRebounds += values[idx].TotalRebounds;
+     reducedVal.BlockedShots += values[idx].BlockedShots;
+     reducedVal.ThreePointMade += values[idx].ThreePointMade;
+     reducedVal.Actions += 1; 
+    }
+    reducedVal.PtsGame = reducedVal.Points /( reducedVal.Actions / 13 ) ;
+    reducedVal.OffensiveReboundsGame = reducedVal.OffensiveRebounds /( reducedVal.Actions / 13 ) ;
+    reducedVal.DefensiveReboundsGame = reducedVal.DefensiveRebounds /( reducedVal.Actions / 13 ) ;
+    reducedVal.TotalReboundsGame = reducedVal.TotalRebounds /( reducedVal.Actions / 13 ) ;
+    reducedVal.BlockedShotsGame = reducedVal.BlockedShots /( reducedVal.Actions / 13 ) ;
+    reducedVal.ThreePointMadeGame = reducedVal.ThreePointMade /( reducedVal.Actions / 13 ) ;
+
+    return reducedVal;
+
+    },
+    {query:{TeamName:req.query.TeamName}, out: { inline: 1 } },
+    function (err, result) {
+
+        if (result) {
+            res.render('EquipeSaison.ejs', {Team: result});
+        }
+    }
+);
+        }
+    else
+        {
+            res.render('EquipeSaison.ejs', {Team:[]});
+        }
+});
+
+
+app.get("/Admin", (req, res) => {
+
+ db.command({ dbStats: 1 }, function (err, result) {
+         console.log(result);
+         res.render('Admin.ejs', {Server: result})
+
+        });
+
+
+});
+
+app.get("/Mvp", (req, res) => {
+
+  
+db.collection('Actions').aggregate([   
+       {
+        $group : {
+           _id : "$PlayerName", 
+           count: { $sum: 1 },
+           Pts:{$sum:"$Points"},
+           TotalRebounds:{ $sum: "$TotalRebounds" },
+           TotalAssists:{ $sum: "$Assists" },
+           TotalSteals:{ $sum: "$Steals" },
+           TotalBlockedShots:{ $sum: "$BlockedShots" },
+        }
+    },
+     { "$addFields": {
+         
+        "PtsGame": { "$divide": ["$Pts", "$count"] },
+        "ReboundsGame": { "$divide": ["$TotalRebounds", "$count"] },
+        "AssistsGame": { "$divide": ["$TotalAssists", "$count"] },
+        "StealsGame": { "$divide": ["$TotalSteals", "$count"] },
+        "BlockedShotsGame": { "$divide": ["$TotalBlockedShots", "$count"] },
+    }},
+    {"$addFields":{
+     "GlobalStats": { "$add": [ "$PtsGame", "$ReboundsGame", "$AssistsGame","$StealsGame","$ReboundsGame", "$BlockedShotsGame"] } 
+
+    }},
+    {
+        "$sort":{GlobalStats:-1}
+        },
+    {
+        "$limit":1
+        }
+    
+ ]).toArray((err, result) => {
+		if (err) return console.log(err)
+		result.SearchedPlayer = req.query.PlayerName;
+		res.render('Mvp.ejs', {Player: result})
+		console.log(result);
+		});
+
+});
+
 /* Post */
 
 
